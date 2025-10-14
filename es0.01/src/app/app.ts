f4ee3743082eb006fb7a5c4f636b48b3388f0bef
turatistefanogalvani <turati.stefano@iisgalvanimi.edu.it>
2025-10-14 10:51:51 +0000

    02.1 da terminare

--- es0.01/src/app/app.ts
+++ es0.01/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { HelloWorld } from './hello-world/hello-world';
 import { UserItemComponent } from './user-item/user-item';
+import { UserListComponent } from './user-list/user-list';
 
 @Component({
   selector: 'app-root',
-  imports: [HelloWorld, UserItemComponent],
+  imports: [HelloWorld, UserItemComponent, UserListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
